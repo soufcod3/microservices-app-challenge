74cff6a95d4c8ea42e5c4e26e16ef5b4e94e7795
soufcod3 <soufiane.ait@outlook.fr>
2023-12-23 20:56:39 +0100

    first commit

---
+++ moderation/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+const handleEvent = async (type, data) => {
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved'
+
+      await axios.post('http://localhost:4005/events', {
+          type: 'CommentModerated',
+          data: {
+              id: data.id,
+              postId: data.postId,
+              status,
+              content: data.content
+          }
+      })
+  } 
+} 
+
+app.post("/events", async (req, res) => {
+
+    const { type, data } = req.body
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content
+            }
+        })
+    }
+    res.send({})
+});
+
+app.listen(4003, async() => {
+  console.log("listening on 4003");
+
+  const res = await axios.get('http://localhost:4005/events')
+
+  for (const event of res.data) {
+      console.log('processing event : ', event.type)
+
+      handleEvent(event.type, event.data)
+  }
+}); 
